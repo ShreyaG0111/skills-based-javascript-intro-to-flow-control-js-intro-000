4ad8ff3a550b6da1748e082079f6deba1bb5d0ed
Shreya Guha <shreya22.sg@gmail.com>
2020-03-11 04:08:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,15 +2,16 @@ function basicTeenager(age) {
 if (age >= 13 && age <=19) {
   return true
 }
-basicTeenager('"You are a teenager!"') // true
+basicTeenager('You are a teenager!') // true
+basicTeenager() // false
 }
 
 function teenager(age) {
   if (age >= 13 && age <=19) {
     return true
   }
-  teenager('"You are a teenager!"') // true
-  teenager('"You are not a teenager"') // false
+  teenager('You are a teenager!') // true
+  teenager('You are not a teenager') // false
 }
 
 function ageChecker(age) {
